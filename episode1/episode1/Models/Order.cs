0566ea3d6339eaaded1b76b9483268eedd5679c6
Anna Madera <anna.madera7@gmail.com>
2021-01-30 17:42:35 +0100

    Episode one

---
+++ episode1/episode1/Models/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Episode1.Models
+{
+    public class Order
+    {
+        public int Id { get; private set; }
+    }
+
+    public Order(int id)
+    {
+        Id = id;
+    }
+}
---
+++ episode1/episode1/Program.cs
@@ -0,0 +1,17 @@
+﻿using Episode1;
+using System;
+
+namespace episode1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            User badUser = new User("test@test.pl","giveMePassword");
+            badUser.Age.Equals(10);
+            badUser.Password = "xyz";
+
+        }
+    }
+}
