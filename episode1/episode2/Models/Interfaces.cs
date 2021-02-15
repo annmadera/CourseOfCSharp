816e20060c9398a51501a286dde4c50657dbc918
Anna Madera <anna.madera7@gmail.com>
2021-02-15 05:40:28 +0100

    add Interface class

--- episode1/episode2/Models/Interfaces.cs
+++ episode1/episode2/Models/Interfaces.cs
@@ -4,7 +4,25 @@ using System.Text;
 
 namespace Episode2.Models
 {
-    class Class1
+    public interface IEmailSender
     {
+        void SendMessage(string receiver, string title, string message);
+
+    }
+
+    public interface IDatabase
+    {
+        bool IsConnected { get; }
+        void Connect();
+        User GetUser(string email);
+        
+
+    }
+    public class EmailSender : IEmailSender
+    {
+        public void SendMessage(string receiver, string title, string message)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
